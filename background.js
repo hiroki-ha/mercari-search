cf81e7c738715aa32a222c98c407d78a8f205964
kkmr <j18357hh@edu.tuis.ac.jp>
2020-07-18 03:59:42 +0900

    [Add] index

--- background.js
+++ background.js
@@ -1,10 +1,15 @@
+function search(info){
+  return function(info){
+    var selection = info.selectionText;
+    var url = "https://www.mercari.com/jp/search/?keyword="+selection;
+    chrome.tabs.create({url : url});
+  }
+}
+
+
 chrome.contextMenus.create({
   "title" : "メルカリで検索",
   "type"  : "normal",
   "contexts" : ["all"],
-  "onclick" : function(info){
-    var selection = info.selectionText;
-    var url = "https://www.mercari.com/jp/search/?keyword="+selection;
-    chrome.tabs.create({url : url});
-}
+  "onclick" : search()
 });
